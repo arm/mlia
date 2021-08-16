c7f1842af73cd35129c3135a6667b9a21f29fbcd
Dmitrii Agibov <dmitrii.agibov@arm.com>
2021-08-16 09:05:40 +0100

    MLIA-137 Emulate Corstone-300 output
    Add emulation for Corstone-300 system/software to speed up development
    lifecycle
    
    Change-Id: I7a9754e33065b352f5798f1a22b250d9b5e0c45a

---
+++ tests/test_resources/aiet/system/corstone300_emulation.py
@@ -0,0 +1,24 @@
+"""Script for the emulation of Corstone-300 inference output."""
+import random
+from functools import partial
+
+
+def emulate_corstone300() -> None:
+    """Emulate Corstone-300 inference output."""
+    random_value = partial(random.randint, 0, 1000)
+
+    output = f"""
+Corstone-300 output emulation.
+
+NPU AXI0_RD_DATA_BEAT_RECEIVED beats: {random_value()}
+NPU AXI0_WR_DATA_BEAT_WRITTEN beats: {random_value()}
+NPU AXI1_RD_DATA_BEAT_RECEIVED beats: {random_value()}
+NPU ACTIVE cycles: {random_value()}
+NPU IDLE cycles: {random_value()}
+NPU TOTAL cycles: {random_value()}
+"""
+    print(output)
+
+
+if __name__ == "__main__":
+    emulate_corstone300()
